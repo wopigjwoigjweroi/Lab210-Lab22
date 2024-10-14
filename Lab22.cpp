62c48c93b3a63a25f2cf54c09f27aa6516011eb0
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-13 20:49:14 -0700

    Commit 2

--- Lab22.cpp
+++ Lab22.cpp
@@ -42,7 +42,6 @@ tail = newNode;
 }
 
 void push_front(int value) {
-  
 Node* newNode = new Node(value);
   
   if (!head) 
@@ -51,52 +50,57 @@ else {
 newNode->next = head;
 head->prev = newNode;
 head = newNode;
+  }
 }
-}
-void insert_after(int value, int position) {
-if (position < 0) {
-cout << "Position must be >= 0." << endl;
-return;
-}
-Node* newNode = new Node(value);
-if (!head) {
-head = tail = newNode;
-return;
-}
-Node* temp = head;
-for (int i = 0; i < position && temp; ++i)
-temp = temp->next;
-if (!temp) {
-cout << "Position exceeds list size. Node not inserted.\n";
-delete newNode;
-return;
-}
-newNode->next = temp->next;
-newNode->prev = temp;
-if (temp->next)
-temp->next->prev = newNode;
-else
-tail = newNode; // Inserting at the end
-temp->next = newNode;
-}
-void delete_node(int value) {
-if (!head) return; // Empty list
-Node* temp = head;
-while (temp && temp->data != value)
-temp = temp->next;
+
+void delete_val(int value) {
+  
+  if (!head) return; // Empty list
+  Node* temp = head;
+  while (temp && temp->data != value)
+  
+  temp = temp->next;
+  
 if (!temp) return; // Value not found
+  
 if (temp->prev) {
-temp->prev->next = temp->next;
+  
+  temp->prev->next = temp->next;
+  
 } else {
-head = temp->next; // Deleting the head
+  
+  head = temp->next; // Deleting the head
 }
+  
 if (temp->next) {
-temp->next->prev = temp->prev;
+  
+  temp->next->prev = temp->prev;
 } else {
-tail = temp->prev; // Deleting the tail
+  
+  tail = temp->prev; // Deleting the tail
 }
-delete temp;
+  
+  delete temp;
 }
+
+void delete_pos() {
+
+
+}
+
+
+
+void pop_front() {
+
+
+
+}
+
+void pop_back() {
+
+
+} 
+
 void print() {
 Node* current = head;
 if (!current) return;
